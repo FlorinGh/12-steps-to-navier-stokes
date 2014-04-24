0afd386b6db9b696cffad2bbd5e2d191234a808d
FlorinGh <florin_gheorghe@ymail.com>
2014-04-24 15:21:00 +0300

    Clarification, comments added and automation

--- step1/1D_LinearConvection.py
+++ step1/1D_LinearConvection.py
@@ -2,15 +2,15 @@
 
 import numpy as np
 import pylab as pl
-pl.ion() # all functions will be plot on the same graph (Matlab hold on)
+pl.ion() # all functions will be ploted on the same graph (Matlab hold on)
 
-D = 2.0 # lenght of the domain
+D = 2.0 # length of the domain
 T = 0.625 # total amount of time for the analysis
 
-nx = 41 # number of grid points
+nx = 31 # number of grid points
 dx = D/(nx-1) # distance between any pair of adjacent grid points
 
-nt = 25 # number of time iterations
+nt = 50 # number of time iterations
 dt = T/nt # amount of time each timestep covers
 
 c = 1.0 # consider a wave speed of c = 1 m/s
@@ -18,8 +18,8 @@ c = 1.0 # consider a wave speed of c = 1 m/s
 grid = np.linspace(0,2,nx) # creating the initial grid
 
 u = np.ones(nx) # creating the initial conditions
-u[0.5/dx:1/dx+1] = 2 # initial velocity is 2 between 0.5-1 and 1 in the rest
-# print the initial conditions; to print them, remove the hashtag
+u[0.5/dx:1/dx+1] = 2.0 # initial velocity is 2 between 0.5-1 and 1 in the rest
+# print the initial conditions; to print them, remove the hashtag in the following
 #print u
 
 # plot the initial conditions; you can save the plot afterwards
@@ -34,7 +34,8 @@ for n in range(nt): # loop for time iteration
     pl.plot(grid,u) # this will plot u for every time step
 
 # Discussion:
-# the wave has moved to the right and changed the shape
+# the wave has moved to the right without changing the shape
+# the hight of the wave is although decreased
 # the hight of the curve represents the velocity of the wave
 # that is the velocity decrease with the pass of time
 # increasing the total time we will make the scheme unstable
--- step2/1D_NonlinearConvection.py
+++ step2/1D_NonlinearConvection.py
@@ -4,22 +4,40 @@
 
 import numpy as np
 import pylab as pl
-pl.ion()
+pl.ion() # all functions will be ploted in the same graph (similar to Matlab hold on)
 
-nx = 41
-dx = 2./(nx-1)
+D = 2.0 # length of the 1D domain
+T = 1.0 # total amount of time for the analysis
 
-nt = 20
-dt = 0.025
+nx = 31 # number of grid points
+dx = D/(nx-1) # distance between any pair of adjacent grid points
+gridx = np.linspace(0,2,nx) # creating the space grid
 
-u = np.ones(nx)
-u[0.5/dx:1/dx+1] = 2.0
+nt = 50 # number of time iterations
+dt = T/nt # duration of each timestep
 
-un = np.ones(nx)
+u = np.ones(nx) # initializing the matrix for velocities
+u[0.5/dx:1/dx+1] = 2.0 # input of initial conditions, same as step1
 
-for n in range(nt):
-    un = u.copy()
-    for i in range(1,nx):
-        u[i] = un[i] - un[i]*dt/dx*(un[i]-un[i-1])
+un = np.ones(nx) # used only to initialize a matrix with the same dimension
+# in here will be kept the velocity for current time step
 
-pl.plot(np.linspace(0,2,nx),u)
+for n in range(nt): # loop for time iteration
+    un = u.copy() # copy the current time step velocity
+    for i in range(1,nx): # loop over the entire space domain
+        u[i] = un[i] - un[i]*dt/dx*(un[i]-un[i-1]) # compute the velocity for
+        # this step for the entire grid based on the velocities in previuos
+        # time step and grid points
+    pl.plot(np.linspace(0,2,nx),u) # the position of this statements
+    # makes the plot to get the curves for each time step
+    
+# Discussion:
+# the wave has moved to the right, with a change in shape
+# in the same time, the hight is decreased
+# increasing the total time will make the scheme unstable if there not enough timesteps
+# that is if the time step is incrreased the problem loose resolution
+
+# as a good recomendation, the time step dt must be corelated with the element size, dx
+# if the size of element, dx is decreased, so should be the timestep
+# the time should be smaller than the time needed for the wave to pass one grid cell
+# for this case, the max velocity is 2, therefore the time step should be less than
