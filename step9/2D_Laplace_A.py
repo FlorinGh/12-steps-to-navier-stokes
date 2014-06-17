ff22bc05e631dfa2774897a2c8c7bd7d4503a29e
FlorinGh <florin_gheorghe@ymail.com>
2014-06-17 10:36:49 +0300

    Step 9 created: Laplace equation
    solved using for loops: Laplace_A

---
+++ step9/2D_Laplace_A.py
@@ -0,0 +1,63 @@
+# Step 9: 2D Laplace Equation
+# this is the homogenous Poisson equation
+# the equation to be solved: d2p/dx2 + d2p/dy2 = 0
+# as it can be seen it is steady state equations and it appears in may applications
+# this code will sole the equation using loops in the style of other steps
+
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import pylab as pl
+from pylab import cm
+pl.ion()
+
+# Variable declaration
+nx = 101  # number of x nodes
+ny = 101  # number of y nodes
+nt = 10000 # maximum number of iterations
+
+dx = 2.0/(nx-1)
+dy = 1.0/(ny-1)
+
+x = np.linspace(0,2,nx)
+y = np.linspace(0,1,ny)
+
+# Initialization
+p = np.zeros((ny,nx))
+pn = np.zeros((ny,nx))
+
+# Assign initial conditions
+p[:,0] = 0
+p[:,-1] = y
+p[0,:] = p[1,:]
+p[-1,:] = p[-2,:]
+
+# Plot the initial conditions
+fig = pl.figure(figsize = (11,7), dpi = 100)
+ax = Axes3D(fig)
+X, Y = np.meshgrid(x,y)
+surf1 = ax.plot_surface(X,Y,p[:], rstride=1, cstride=1, cmap=cm.coolwarm, 
+linewidth=0, antialiased=True)
+ax.view_init(30,225)
+
+# Applying the scheme to determine the pressure distributions
+l1norm_target = 0.00001
+for n in range(nt+1):
+    pn = p.copy()
+    p[1:-1,1:-1] = (dx**2*(pn[1:-1,0:-2] + pn[1:-1,2:]) + \
+    dy**2*(pn[0:-2,1:-1] + pn[2:,1:-1]))/(2*dx**2+2*dy**2)
+    
+    p[:,0] = 0
+    p[:,-1] = y
+    p[0,:] = p[1,:]
+    p[-1,:] = p[-2,:]
+    
+    l1norm = (np.sum(np.abs(p[:])-np.abs(pn[:])))/np.sum(np.abs(pn[:]))
+    if l1norm < l1norm_target:
+        break
+
+# Plot the results
+fig = pl.figure(figsize = (11,7), dpi = 100)
+ax = Axes3D(fig)
+surf2 = ax.plot_surface(X,Y, p[:], rstride=1, cstride=1, cmap=cm.coolwarm, 
+linewidth=0, antialiased=True)
+ax.view_init(30,225)
---
+++ step9/2D_Laplace_B.py
@@ -0,0 +1,5 @@
+# Step 9: 2D Laplace Equation
+# this is the homogenuous Poisson equation
+# the equation to be solved: d2p/dx2 + d2p/dy2 = 0
+# as it can be seen it is steady state equations and it appears in may applications
+
