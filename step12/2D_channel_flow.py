09c4a76a5b62f285cb3bcbd7028aae67b484557e
FlorinGh <florin_gheorghe@ymail.com>
2014-06-19 14:21:15 +0300

    Step 12 created: Channel Flow

---
+++ step12/2D_channel_flow.py
@@ -0,0 +1,27 @@
+# Step 12: 2D Channel Flow
+# this another problem solved with the Navier Stokes Eq
+# this time the domain is a channel
+# and in the U momentum equation we have a source term which makes the fluid
+# to move form left to right
+# introducing a periodic boundary condition
+
+import numpy as np
+import pylab as pl
+from pylab import cm
+pl.ion()
+
+# The problem will be solve by modules, using functions
+
+# Defininig the function for pressure build-up
+
+
+
+
+
+
+
+
+
+
+
+
